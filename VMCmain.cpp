39fae99ed1b6c9353fca6bb972eba3133361275f
FrancescoSlongo <Slongofrancesco@gmail.com>
2019-04-30 23:36:06 +0200

    Aggiunto print txt

--- VMCmain.cpp
+++ VMCmain.cpp
@@ -10,7 +10,7 @@
 #include "functions.h"			// Nostra libreria con funzioni
 
 using namespace std;  // In C++ si lavora in ambienti, chiamando all'inizio si evita il problema ed � come in C
-
+ofstream ofile; // Ambiente print in txt
 int main() {
 	// Chiamo il tutto il necessario per il generatore di numeri casuali
 	random_device rd;
@@ -23,10 +23,15 @@ int main() {
 	// 1 -> wavefunction gaussiana con solo alpha e con interazione culombiana
 	// 2 -> wavefunction con due parametri e interazione culombiana
 
+	// Nome file
+	string outname = "VMC_";
+	outname.append(to_string(wftype));
+	outname.append(".txt");
+
 	// inizializzo variabili per la fisica della simulazione
 	double alpha = 0.7; // Paramentro funzione d'onda: sigma gaussiana
 	double beta = 0.15; // Parametro funzione d'onda: Jastrow term
-	double omega = 1; // Frequenza dell'oscillatore armonico
+	double omega[4] = { 0.01, 0.5, 1, 5}; // Frequenza dell'oscillatore armonico
 	double n_beta = 10; // Numero di beta visitati nel ciclo
 	double n_alpha = 5; // Numero di alpha visitati nel ciclo
 	double step_beta = 0.01; // incremento di beta
@@ -53,73 +58,80 @@ int main() {
 	// Funzioni d'onda calcolanata nel punto
 	double Psiold = 0, Psinew = 0;
 
-	// Ciclo su diversi valori di beta
-	for (int j = 0; j < n_beta; j++) {
-		cout << "Beta " << beta << endl;
-		// Ciclo su diversi valori di alpha
-		for (int i = 0; i < n_alpha; i++) {
+	ofile.open(outname);
+	// Ciclo su diversi valori di omega
+	for (int q = 0; q < 4; q++) {
+		// Ciclo su diversi valori di beta
+		for (int p = 0; p < n_beta; p++) {
+			cout << "Omega " << omega[q] << "Beta " << beta << endl;
+			// Ciclo su diversi valori di alpha
+			for (int s = 0; s < n_alpha; s++) {
 
-			// Definisco rstep in funzione di alpha
-			rstep = 1.5 / sqrt(alpha * omega);
-			// Calcolo la wavefunction nel punto
-			Psiold = Wavefunction(Rold, omega, alpha, beta, wftype);
-			for (int j = 0; j < MCC; j++) { // Ciclo sui MCC
-				// Nuovo valore posizioni
-				for (int i = 0; i < 6; i++) {
-					Rnew[i] = Rold[i] + rstep * (RNG(gen) - 0.5);
-				}
+				// Definisco rstep in funzione di alpha
+				rstep = 1.5 / sqrt(alpha * omega[q]);
 				// Calcolo la wavefunction nel punto
-				Psinew = Wavefunction(Rnew, omega, alpha, beta, wftype);
-
-				// Metropolis
-				if (RNG(gen) <= Psinew * Psinew / (Psiold * Psiold)) {  // Accetto la mossa
+				Psiold = Wavefunction(Rold, omega[q], alpha, beta, wftype);
+				for (int j = 0; j < MCC; j++) { // Ciclo sui MCC
+					// Nuovo valore posizioni
 					for (int i = 0; i < 6; i++) {
-						Rold[i] = Rnew[i];
+						Rnew[i] = Rold[i] + rstep * (RNG(gen) - 0.5);
 					}
-					Psiold = Psinew;
-					accratio += 1;
-				}
-				else { // Rifiuto la mossa
-					for (int i = 0; i < 6; i++) {
-						Rnew[i] = Rold[i];
+					// Calcolo la wavefunction nel punto
+					Psinew = Wavefunction(Rnew, omega[q], alpha, beta, wftype);
+
+					// Metropolis
+					if (RNG(gen) <= Psinew * Psinew / (Psiold * Psiold)) {  // Accetto la mossa
+						for (int i = 0; i < 6; i++) {
+							Rold[i] = Rnew[i];
+						}
+						Psiold = Psinew;
+						accratio += 1;
 					}
-				}
-				if (j > ncut) {
-					E = Localenergy(Rnew, omega, alpha, beta, wftype);
-					sumE += E;
-					sumE2 += E * E;
-				}
+					else { // Rifiuto la mossa
+						for (int i = 0; i < 6; i++) {
+							Rnew[i] = Rold[i];
+						}
+					}
+					if (j > ncut) {
+						E = Localenergy(Rnew, omega[q], alpha, beta, wftype);
+						sumE += E;
+						sumE2 += E * E;
+					}
 
+				}
+				// Medio i risultati
+				sumE = sumE / (MCC - ncut); // Medio i valori
+				sumE2 = sumE2 / (MCC - ncut);
+				errore = sqrt(sumE2 - sumE * sumE);
+				accratio = accratio / (MCC - ncut);
+				// Print a schermo dei risultati
+				cout << "Omega" << omega[q] << " Alpha " << alpha << " Media " << sumE << " Errore (1 sigma) " << errore << " Percentuale di accettazione " << accratio * 100 << "%" << endl;
+				// Print a schermo dei risultati
+				ofile << "Omega" << omega[q] << " Alpha " << alpha << " Beta " << beta << " Media " << sumE << " Errore (1 sigma) " << errore << " Percentuale di accettazione " << accratio * 100 << "%" << endl;
+				// Trovo l'energia minima
+				if (sumE < Emin) {
+					Emin = sumE;
+					errore_Emin = errore;
+					betamin = beta;
+					alphamin = alpha;
+				}
+				// Resetto i valori per un nuovo alpha
+				sumE = 0;
+				sumE2 = 0;
+				accratio = 0;
+				alpha += step_alpha;
 			}
-			// Medio i risultati
-			sumE = sumE / (MCC - ncut); // Medio i valori
-			sumE2 = sumE2 / (MCC - ncut);
-			errore = sqrt(sumE2 - sumE * sumE);
-			accratio = accratio / (MCC - ncut);
-			// Print a schermo dei risultati
-			cout << "Alpha " << alpha << " Media " << sumE << " Errore (1 sigma) " << errore << " Percentuale di accettazione " << accratio * 100 << "%" << endl; // Print a terminale
-			// Trovo l'energia minima
-			if (sumE < Emin) {
-				Emin = sumE;
-				errore_Emin = errore;
-				betamin = beta;
-				alphamin = alpha;
-			}
-			// Resetto i valori per un nuovo alpha
-			sumE = 0;
-			sumE2 = 0;
-			accratio = 0;
-			alpha += step_alpha;
-		}
-		// resetto il valore di beta
-		beta += step_beta;
-		alpha = alpha - n_alpha * step_alpha;
+			// resetto il valore di beta
+			beta += step_beta;
+			alpha = alpha - n_alpha * step_alpha;
 
-		// Se non mi serve beta esco dal ciclo su beta
-		if (wftype != 2) {
-			j = n_beta;
+			// Se non mi serve beta esco dal ciclo su beta
+			if (wftype != 2) {
+				p = n_beta;
+			}
 		}
 	}
+	ofile.close();
 	cout << "Energia minima " << Emin << " errore (1 sigma) " << errore_Emin << " beta " << betamin << " alpha " << alphamin << endl;
 	system("pause"); // Comando che non fa chiudere in automatico la finestra di output in Visual Studio
 	
